8fff68ca91e85c363b91b5ff640dfcd9124389d4
jsunwoo <jinsung.sunwoo@gmail.com>
2018-09-20 00:47:21 +0900

    Fix the bug (region 을 MapView<>안에 넣지도 않고 실행시키고 있었다)

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
-import { ExpoLinksView } from "@expo/samples";
+import { View, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -8,14 +8,37 @@ export default class LinksScreen extends React.Component {
   };
 
   render() {
-    return <ScrollView style={styles.container} />;
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: 59.32932349999999,
+            longitude: 18.06858080000063,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1
+          }}
+        />
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: "#fff"
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    justifyContent: "flex-end",
+    alignItems: "center"
+  },
+  map: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0
   }
 });
